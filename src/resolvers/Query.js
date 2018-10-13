472b8dd07497eeef6664a303b4525571e731624a
Gustavo Silva <gustafsilva@hotmail.com>
2018-10-13 16:30:30 -0300

    Adding filter and order for feed :dart:

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,7 +3,23 @@ const info = () => {
 }
 
 const feed = (parent, args, context, info) => {
-  return context.db.query.links({}, info);
+  let where = {};
+
+  if(args.filter) {
+    where = {
+      OR: [
+        { "url_contains": args.filter },
+        { "description_contains": args.filter }
+      ]
+    }
+  }
+
+  return context.db.query.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy
+  }, info);
 };
 
 module.exports = {
